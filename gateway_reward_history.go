7c624be496c5f7967b7342d0b907029a934005e6
Bas van Kervel <basvankervel@gmail.com>
2023-04-12 13:04:47 +0200

    Add custom json marshal to gateway and router reward types

--- gateway_reward_history.go
+++ gateway_reward_history.go
@@ -1,6 +1,23 @@
+// Copyright 2023 Stichting ThingsIX Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package types
 
 import (
+	"encoding/json"
 	"math/big"
 	"time"
 )
@@ -19,4 +36,13 @@ type GatewayRewardHistory struct {
 
 	// The reward in THIX "gweis" for this gateway
 	Rewards *big.Int `json:"rewards"`
+}
+
+func (grh GatewayRewardHistory) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"gatewayId":            grh.GatewayID,
+		"date":                 grh.Date,
+		"assumedCoverageUnits": "0x" + grh.AssumedCoverageShareUnits.Text(16),
+		"rewards":              "0x" + grh.Rewards.Text(16),
+	})
 }
--- mapper_reward_history.go
+++ mapper_reward_history.go
@@ -1,6 +1,23 @@
+// Copyright 2023 Stichting ThingsIX Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package types
 
 import (
+	"encoding/json"
 	"math/big"
 	"time"
 )
@@ -19,4 +36,13 @@ type MapperRewardHistory struct {
 
 	// The reward in THIX "gweis" for this mapper
 	Rewards *big.Int `json:"rewards"`
+}
+
+func (mrh MapperRewardHistory) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"mapperId":     mrh.MapperID,
+		"date":         mrh.Date,
+		"mappingUnits": "0x" + mrh.MappingUnits.Text(16),
+		"rewards":      "0x" + mrh.Rewards.Text(16),
+	})
 }
